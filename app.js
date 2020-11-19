67ed056bea34aea89d3440d2ebf06b9c22ff6b80
EC2 Default User <ec2-user@ip-172-31-25-56.us-west-1.compute.internal>
2020-11-19 05:03:16 +0000

    Initial commit of Planets site

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get("/earth", function(req, res) {
     res.render("earth.html");
 });
 
+app.get("/planetX", function(req, res) {
+    res.render("planetX.html");
+});
+
 //starting server
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Express server is running...")
